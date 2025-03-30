8188ec3f184a1a196888b637574c6a6b8ace2221
Sergey Ivanov <SVIvanov@yandex.ru>
2025-03-30 12:54:14 +0300

    refactoring flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = os.getenv('DEBUG', 'True')
 
-ALLOWED_HOSTS = ['158.160.5.127', '127.0.0.1', 'localhost', 'practicumsvivanov.ddns.net']
+ALLOWED_HOSTS = ['158.160.5.127', '127.0.0.1',
+                 'localhost', 'practicumsvivanov.ddns.net']
 
 
 # Application definition
